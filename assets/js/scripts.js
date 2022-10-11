2baa4273981762b20c9e14afac3d9ab94cab583d
Aldevan Silva <aldevan6@gmail.com>
2022-10-11 11:51:30 -0300

    Adicionou animações e chat

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,19 +1,27 @@
-let btnTopo = document.querySelector('btn-topo');
+let btnTopo = document.querySelector('.btn-topo');
 // Animação do menu
 let navbarTop = document.querySelector('.navbar-top');
 let header = document.querySelector('header');
 
 window.addEventListener('scroll', ()=>{
-    if (window.scrollY > 100) {
+    if (window.scrollY > 80) {
         navbarTop.classList.add('anime-header');
-        header.style.position = "fixed";
+        header.style.position = 'fixed';
         header.style.zIndex = 200;
+        btnTopo.style.display = 'flex'
     }else{
         navbarTop.classList.remove('anime-header');
+        btnTopo.style.display = 'none';
     }
 });
 
 // Fim animação do menu
 
-
-
+//Botão para voltar ao topo inicio
+function subirTela(){
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+//Fim
